0a5b5546d19a396274d9ea82733c5e5e74459739
Rikopernando <rikopernando96@gmail.com>
2017-03-31 21:07:01 +0700

    perbaikan transaksi stok opname
    perbaikan transaksi stok opname copy dari surya

--- proses_tbs_stok_opname.php
+++ proses_tbs_stok_opname.php
@@ -3,11 +3,13 @@
 include 'sanitasi.php';
 include 'db.php';
     // mengirim data sesuai variabel yang ada dengan menggunakan metode POST
-    $no_faktur = $_POST['no_faktur'];
-    $kode_barang = $_POST['kode_barang'];
-    $nama_barang = $_POST['nama_barang'];
-    $satuan = $_POST['satuan'];
-    $jumlah_fisik = $_POST['fisik'];
+    $kode_barang = stringdoang($_POST['kode_barang']);
+    $nama_barang = stringdoang($_POST['nama_barang']);
+    $satuan = angkadoang($_POST['satuan']);
+    $jumlah_fisik = angkadoang($_POST['fisik']);
+    $jumlah_stok_komputer = angkadoang($_POST['jumlah_stok_komputer']);
+    $jumlah_hpp_minus = angkadoang($_POST['jumlah_hpp_minus']);
+    $jumlah_hpp_plus = angkadoang($_POST['jumlah_hpp_plus']);
     
 
         $query9 = $db->prepare("UPDATE barang SET stok_opname = 'ya' WHERE kode_barang = ?");
@@ -42,79 +44,29 @@ include 'db.php';
         $jumlah_keluar_item_keluar = $hasil4['keluar'];
 
         $hasil_keluar = $jumlah_keluar_penjualan + $jumlah_keluar_item_keluar;
+                  
+        $selisih_fisik = $jumlah_fisik - $jumlah_stok_komputer;
 
-
-
-        $query6 = $db->query("SELECT * FROM barang WHERE kode_barang = '$kode_barang'");
-
-        $select = $db->query("SELECT SUM(jumlah_kuantitas) AS jumlah_hpp_masuk FROM hpp_masuk WHERE kode_barang = '$kode_barang'");
-             $ambil_masuk = mysqli_fetch_array($select);
-             
-             $select_2 = $db->query("SELECT SUM(jumlah_kuantitas) AS jumlah_hpp_keluar FROM hpp_keluar WHERE kode_barang = '$kode_barang'");
-             $ambil_keluar = mysqli_fetch_array($select_2);
-             
-             $jumlah_stok_komputer = $ambil_masuk['jumlah_hpp_masuk'] - $ambil_keluar['jumlah_hpp_keluar'];
-            
-            
-            
-            $selisih_fisik = $jumlah_fisik - $jumlah_stok_komputer;
-
-    if ($selisih_fisik < 0) {
-
-       // HARGA DARI HPP MASUK
-
-       $pilih_hpp = $db->query("SELECT harga_unit FROM hpp_masuk WHERE kode_barang = '$kode_barang' ORDER BY id DESC LIMIT 1");
-       $ambil_hpp = mysqli_fetch_array($pilih_hpp);
-       $jumlah_hpp = $ambil_hpp['harga_unit'];
-
-
-       // SAMPAI SINI
-
-    } 
-
-    else {
-
-              // HARGA DARI DETAIL PEMBELIAN ATAU BARANG
-        
-        $select2 = $db->query("SELECT harga FROM detail_pembelian WHERE kode_barang = '$kode_barang' ORDER BY id DESC LIMIT 1");
-        $num_rows = mysqli_num_rows($select2);
-        $fetc_array = mysqli_fetch_array($select2);
-        
-        $select3 = $db->query("SELECT harga_beli FROM barang WHERE kode_barang = '$kode_barang' ORDER BY id DESC LIMIT 1");
-        $ambil_barang = mysqli_fetch_array($select3);
-        
-        if ($num_rows == 0) {
-        
-        $jumlah_hpp = $ambil_barang['harga_beli'];
+        if ($selisih_fisik < 0) {
+          $jumlah_hpp = $jumlah_hpp_minus;
+        $selisih_harga = $jumlah_hpp_minus * $selisih_fisik;    
         
         } 
         
         else {
-        
-        $jumlah_hpp = $fetc_array['harga'];
-        
+
+          $jumlah_hpp = $jumlah_hpp_plus;
+          $selisih_harga = $jumlah_hpp_plus * $selisih_fisik;    
         }
-        
-        // SAMPAI SINI
 
 
-    }
-    
-
-        
-     $selisih_harga = $jumlah_hpp * $selisih_fisik;      
-        
-        
-        
-        $cek = $db->query("SELECT * FROM stok_awal WHERE kode_barang = '$kode_barang' ");
+        $cek = $db->query("SELECT jumlah_awal FROM stok_awal WHERE kode_barang = '$kode_barang' ");
         $data = mysqli_fetch_array($cek);
         $stok_awal = $data['jumlah_awal'];
         
         
-        $query = "INSERT INTO tbs_stok_opname (no_faktur, kode_barang, nama_barang, satuan, awal, masuk, keluar, stok_sekarang, fisik, selisih_fisik, selisih_harga, harga, hpp) 
-        VALUES ('$no_faktur', '$kode_barang','$nama_barang','$satuan','$stok_awal','$hasil_masuk','$hasil_keluar','$jumlah_stok_komputer','$jumlah_fisik','$selisih_fisik','$selisih_harga','$jumlah_hpp','$jumlah_hpp')";
-      
-
+        $query = "INSERT INTO tbs_stok_opname (kode_barang, nama_barang, satuan, awal, masuk, keluar, stok_sekarang, fisik, selisih_fisik, selisih_harga, harga, hpp) 
+        VALUES ('$kode_barang','$nama_barang','$satuan','$stok_awal','$hasil_masuk','$hasil_keluar','$jumlah_stok_komputer','$jumlah_fisik','$selisih_fisik','$selisih_harga','$jumlah_hpp','$jumlah_hpp')";
         
         if ($db->query($query) === TRUE)
         {
@@ -124,61 +76,7 @@ include 'db.php';
         {
             echo "Error: " . $query . "<br>" . $db->error;
         }
-  
-    ?>
-<!-- membuat agar ada garis pada tabel, disetiap kolom -->
-                  <table id="tableuser" class="table table-bordered">
-                  <thead>
-                  
-                  <th> Nomor Faktur </th>
-                  <th> Kode Barang </th>
-                  <th> Nama Barang </th>
-                  <th> Satuan </th>
-                  <th> Stok Komputer </th>
-                  <th> Jumlah Fisik </th>
-                  <th> Selisih Fisik </th>
-                  <th> Hpp </th>
-                  <th> Selisih Harga </th>
-                  <th> Harga </th>
-                  <th> Hapus </th>
-                  
-                  </thead>
-                  
-                  <tbody>
-                  <?php
-                  
-                  
-      $perintah = $db->query("SELECT * FROM tbs_stok_opname");
-                  
-                  //menyimpan data sementara yang ada pada $perintah
-                  while ($data1 = mysqli_fetch_array($perintah))
-                  {
-                  
-                  
-                  echo "<tr>
-                  
-                  <td>". $data1['no_faktur'] ."</td>
-                  <td>". $data1['kode_barang'] ."</td>
-                  <td>". $data1['nama_barang'] ."</td>
-                  <td>". $data1['satuan'] ."</td>
-                  <td><span id='text-stok-sekarang-".$data1['id']."'>". rp($data1['stok_sekarang']) ."</span></td>
-
-                  <td class='edit-jumlah' data-id='".$data1['id']."'><span id='text-jumlah-".$data1['id']."'>". $data1['fisik'] ."</span> <input type='hidden' id='input-jumlah-".$data1['id']."' value='".$data1['fisik']."' class='input_jumlah' data-id='".$data1['id']."' autofocus='' data-faktur='".$data1['no_faktur']."' data-harga='".$data1['harga']."' data-kode='".$data1['kode_barang']."' data-selisih-fisik='".$data1['selisih_fisik']."' data-stok-sekarang='".$data1['stok_sekarang']."'> </td>
-
-                  <td><span id='text-selisih-fisik-".$data1['id']."'>". rp($data1['selisih_fisik']) ."</span></td>
-                  <td><span id='text-hpp-".$data1['id']."'>". rp($data1['hpp']) ."</span></td>
-                  <td><span id='text-selisih-".$data1['id']."'>". rp($data1['selisih_harga']) ."</span></td>
-                  <td>". rp($data1['harga']) ."</td>
-                  
-                  <td> <button class='btn btn-danger btn-hapus' data-id='". $data1['id'] ."' data-kode-barang='". $data1['kode_barang'] ."' data-nama-barang='". $data1['nama_barang'] ."'> <span class='glyphicon glyphicon-trash'> </span> Hapus </button> </td> 
-
-
-                  </tr>";
-                  }
-
-                  //Untuk Memutuskan Koneksi Ke Database
+             
+    
                   mysqli_close($db);   
                   ?>
-                  </tbody>
-                  
-                  </table>
