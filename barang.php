fecb5e4e87b3bcdb2d455a752e3542efd423a435
fahrizalrahman <fahrizal95rhm@gmail.com>
2017-07-18 12:01:41 +0700

    Perbaikan Bug & Request
    - lupa hidden

--- barang.php
+++ barang.php
@@ -404,7 +404,7 @@ echo '<br><button type="button" class="btn btn-info" data-toggle="modal" data-ta
     <div class="form-group">
         <label> Nama Barang :</label>
         <input type="text" id="data_barang" class="form-control" autocomplete="off" readonly=""> 
-        <input type="text" id="id_hapus" class="form-control" > 
+        <input type="hidden" id="id_hapus" class="form-control" > 
                 <input type="hidden" id="kode_barang_hapus" class="form-control" > 
 
     </div>
