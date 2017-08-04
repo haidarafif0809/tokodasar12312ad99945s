4bae63263890950ca0deb5f95af04c0d8f0c353a
Rikopernando <rikopernando96@gmail.com>
2017-08-04 10:26:56 +0700

    tambah link untuk surat jalan
    tambah link untuk surat jalan

--- datatable_penjualan.php
+++ datatable_penjualan.php
@@ -144,7 +144,9 @@ if ($row['status'] == 'Lunas') {
 				<button class='btn btn-primary dropdown-toggle' type='button' data-toggle='dropdown' style='width:150px'> Cetak Penjualan <span class='caret'></span></button>
 				
 				<ul class='dropdown-menu'>
-				<li><a href='cetak_lap_penjualan_tunai.php?no_faktur=".$row['no_faktur']."' target='blank'> Cetak Penjualan </a></li> 				<li><a href='cetak_lap_penjualan_tunai_besar.php?no_faktur=".$row['no_faktur']."' target='blank'> Cetak Penjualan Besar </a></li>
+				<li><a href='cetak_lap_penjualan_tunai.php?no_faktur=".$row['no_faktur']."' target='blank'> Cetak Penjualan </a></li> 
+				<li><a href='cetak_lap_penjualan_tunai_besar.php?no_faktur=".$row['no_faktur']."' target='blank'> Cetak Penjualan Besar </a></li>
+				<li><a href='cetak_penjualan_surat_jalan.php?no_faktur=".$row['no_faktur']."' target='blank'> Cetak Surat Jalan </a></li>
 				</ul>
 				</div>";
 }
